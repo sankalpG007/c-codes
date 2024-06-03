320b468168b66d2beb3b53914f7e2afa72ac13d6
Sankalp Singh <118524818+sankalpG007@users.noreply.github.com>
2024-06-03 12:26:45 +0530

    Add files via upload
    datatypes example

---
+++ datatypes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    a=12;
+
+    cout<<"size of int " <<sizeof(a)<<endl;
+
+    float b;
+    cout<<"size of float " <<sizeof(b)<<endl;
+
+    double c;
+    cout<<"size of double " <<sizeof(c)<<endl;
+
+    bool d;
+    cout<<"size of bool " <<sizeof(d)<<endl;
+
+
+    return 0;
+}
