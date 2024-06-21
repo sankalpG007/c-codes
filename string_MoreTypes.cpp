389f9b145dd740b6746ffd68cae9c1467292a5d6
Sankalp Singh <118524818+sankalpG007@users.noreply.github.com>
2024-06-21 23:36:55 +0530

    Add files via upload
    string more types of string functions

---
+++ string_MoreTypes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+     string s1="900";
+     
+     
+     //cout<<s1.find("or")<<endl; 
+     //cout<<s1.insert(4,"gym")<<endl;
+     int x = stoi(s1);
+    cout<<x+2<<endl;
+
+    return 0;
+}
