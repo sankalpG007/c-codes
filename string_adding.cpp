226e4f3ebb47a07c693c327b5869485f3c762e25
Sankalp Singh <118524818+sankalpG007@users.noreply.github.com>
2024-06-21 23:34:03 +0530

    Add files via upload
    string adding char/int

---
+++ string_adding.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+     string s1="Bench ";
+     string s2="press";
+
+     s1 = s1 + s2;
+     cout<<s1<<endl;
+
+    return 0;
+}
