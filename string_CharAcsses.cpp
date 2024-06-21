9ad51186f118ba95bb6d67b368e33128c904e166
Sankalp Singh <118524818+sankalpG007@users.noreply.github.com>
2024-06-21 23:34:30 +0530

    Add files via upload
    string character access

---
+++ string_CharAcsses.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+     string s1="Bench ";
+     string s2="press";
+
+     //s1 = s1 + s2;
+     //cout<<s1<<endl;
+     cout<<s1[4]<<endl;
+
+    return 0;
+}
