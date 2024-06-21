11fd50dce06c9f6e00a762c28a15677f957ffd46
Sankalp Singh <118524818+sankalpG007@users.noreply.github.com>
2024-06-21 23:35:18 +0530

    Add files via upload
    string clear fuction

---
+++ string_clear.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+     string s1="gym";
+     
+     cout<<s1<<endl;
+
+   if(s1.empty()){
+
+    cout<<"string is empty";
+   }else{
+    cout<<"string is not empty";
+   }
+
+    return 0;
+}
