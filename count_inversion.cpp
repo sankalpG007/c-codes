cabc7ec816a82842e9fbe2a69bca436b7ef96aa3
Sankalp Singh <118524818+sankalpG007@users.noreply.github.com>
2024-06-28 23:54:24 +0530

    Add files via upload
    count inversion

---
+++ count_inversion.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+long long merge (int arr[],int l,int mid,int r)
+{
+    long long inv = 0;
+    int n1 = mid -1+1;
+    int n2 = r - mid;
+    int a[n1];
+    int b[n2];
+
+for (int i = 0; i < n1; i++)
+{
+    a[i] = arr[l+i];
+}
+
+for (int i = 0; i < n2; i++)
+{
+    b[i] = arr[mid+i+l]
+}
+
+int i =0,j=0,k=1;
+while (i<n1 and j<n2)
+{
+    if (a[i]<=b[j])
+    {
+        arr[k] = a[i];
+        k++;i++;
+    }
+    else{
+        arr[k]= b[j];
+        inv += n1-1;
+        k++;j++
+    }
+    
+}
+
+while(i<n1){
+    arr[k] = a[i];
+    k++;i++;
+}
+    
+}
